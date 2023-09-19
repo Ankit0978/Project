9ea9b154a9da51acb86771f485b69fee7a909ee2
Ankit Jha (BlackBeltHelp) <ankit_jha@blackbelthelp.com>
2023-09-19 20:06:14 +0530

    Javascript

---
+++ Variables.js
@@ -0,0 +1,19 @@
+// var b ="chinku";
+// console.log(b)
+
+// let a = 67;
+
+// console.log(a)
+// let a ="harry";
+// console.log(a)
+
+// var b ="36";
+// console.log(b)
+
+const author ="Ankit";
+console.log(author)
+
+let author="Shivam";
+console.log(author)
+
+
