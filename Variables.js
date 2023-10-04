761ab3633ac5f4c3f899b1d7bc6d4e751f66cc3c
Ankit Jha (BlackBeltHelp) <ankit_jha@blackbelthelp.com>
2023-10-04 21:33:46 +0530

    4oct

--- Variables.js
+++ Variables.js
@@ -97,14 +97,156 @@
 // let num2 = "23";
 // console.log(num1 !== num2 )
 
-let num = prompt("Enter the number")
+// let num = prompt("Enter the number")
 
-if(num%2==0){
-    console.log("Number is even")
-}
-else{
-    console.log("Number is odd")
-}
+// if(num%2==0){
+//     console.log("Number is even")
+// }
+// else{
+//     console.log("Number is odd")
+// }
 
+// let firstname = "";
+// if(firstname){
+//     console.log(firstname);
+// }
+// else{
+//     console.log("This is a falsy value")
+// }
+
+// let age = prompt("Enter the number")
+// let drink;
+// if(age>=5){
+//     drink = "cofee"
+// }
+// else{
+//     drink = "milk"
+// }
+// console.log(drink)
+
+
+// let age = 2
+// let drink = age>=5 ? "beer" : "milk"
+// console.log(drink)
+
+// let name1 = "Ankit"
+// let age = 16
+// if (name1[0]==="A" || age>18){
+//     console.log("Your name is ankit and age is above 18")
+// }
+// else(
+//     console.log("Your age is not deifned")
+// )
+
+
+// Nested - if -else
+
+// let winning_number = 19
+// let guess_number = +prompt("Enter your guess number")
+// if (guess_number === winning_number){
+//     console.log("Your guess is right")
+// }
+// else{
+//     if(guess_number<winning_number){
+//     console.log("too low!!!!")
+//     }
+//     else{
+
+//     console.log("too high")
+//     }
+// }
+
+// if
+// else-if
+// else
+
+// let temp = 46;
+// if (temp<0){
+//     console.log("extremely cold outside")
+// }
+// else if(temp<16){
+//     console.log("good cold around")
+// }
+// else if(temp<26){
+//     console.log("pleasant weather outside")
+// }
+// else if(temp<36){
+//     console.log("Hot outside")
+// }
+// else{
+//     console.log("Very hot outside")
+// }
+
+
+// let day = 9;
+
+// switch(day){
+//     case 0:
+//         console.log("Sunday");
+//         break
+//     case 1:
+//         console.log("Monday");
+//         break
+
+//     case 2:
+//         console.log("Tuesday");
+//         break
+
+//     case 3:
+//         console.log("Wednesday");
+//         break
+
+//     case 4:
+//         console.log("Thursday");
+//         break
+
+//     case 5:
+//         console.log("Friday")
+//         break
+
+//     case 6:
+//         console.log("Saturday");
+//         break
+
+//     default:
+//         console.log("Invalid day")
+        
+// }
+
+
+// while loop
+
+// let num = 0
+
+// while(num<10){
+//     console.log(num)
+//     num++
+// }
+
+// let num = 0
+// let sum = 0
+// while(num<0){
+//     sum += sum +num
+// }
+
+// const number = parseInt(prompt("Enter the number"))
+
+// let sum = 0
+// for (let i =1; i<=number; i ++){
+//     sum +=i
+// }
+// console.log("The sum of natural numbers is ", sum)
+
+
+// let num = 100;
+
+// let total = (num*(num+1))/2;
+// console.log(total);
+
+
+for (let i =0;i<=9;i ++){
+    console.log(i)
+}
+console.log("value of i is ", i)
 
 
