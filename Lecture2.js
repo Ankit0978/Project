a3dbf04e597947c3de4d05794337a3dc4535030e
Ankit Jha (BlackBeltHelp) <ankit_jha@blackbelthelp.com>
2023-10-12 21:39:37 +0530

    Arrays

--- Lecture2.js
+++ Lecture2.js
@@ -182,13 +182,104 @@
 // Object Clone - Method 3
 
 
-let src = {
-    a:10,
-    b:20,
-    c:30
-}
-let dest = {...src}
-console.log(dest)
+// let src = {
+//     a:10,
+//     b:20,
+//     c:30
+// }
+// let dest = {...src}
+// console.log(dest)
 
-src.a++
-console.log(dest)
+// src.a++
+// console.log(dest)
+
+// Different methods in Js
+
+// Math.abs(2.33)
+
+
+// let lastname = "   Babbar   "
+
+// console.log(lastname)
+
+// lastname.
+// console.log(typeof(lastname))
+
+// Object String
+// let firstname = new String ("love")
+// console.log(firstname)
+// console.log(typeof(firstname))
+
+//Template literal
+// let message = "This is my first message"
+// console.log(` This ${lastname}
+// is
+// a 
+// template
+// literal`)
+
+// Date and Time
+// let date = new Date();
+// console.log(date)
+
+// let date2 = new Date("June 20 1998 07:15")
+// console.log(date2)
+
+// let date3 = new Date(1998, 11, 20, 7)
+// console.log(date3)
+// date3.setFullYear(1947)
+// console.log(date3)
+// date3.setMonth(8)
+// console.log(date3)
+
+// date3.getMonth
+// console.log(date3)
+
+// Adding elements in an array
+// let numbers = [1,3,5,7]
+// console.log(numbers)
+
+// console.log(numbers[1])
+// console.log(numbers[3])
+// numbers.push(9) // push() - used to add element in the last of the array 
+// numbers.push(8)
+// console.log(numbers)
+
+// numbers.unshift(2) // unshift() - used to add element in the starting of the array
+// console.log(numbers)
+
+// numbers.splice(3,0,"a","b","c") // splice - used to add element in between of the array
+// console.log(numbers) 
+
+// Check whether a number is present in an array
+//  To check if a given number is present in the list or not
+// if(numbers.indexOf(3) != -1)
+//     console.log("present")
+
+// console.log(numbers.includes(7))
+
+// Arrow Function
+// let courses = [
+//     {no:1, naam:"Ankit"},
+//     {no:2, naam:"Love"}
+// ]
+// // console.log(courses)
+
+// // console.log(courses.includes({no:1,naam:"Ankit"}))
+
+//    let course= courses.find(course => course.naam === "Ankit");
+    
+// console.log(course)
+
+// Delete elements in an array
+
+let numbers = [1,2,3,44,4]
+
+numbers.pop();
+console.log(numbers)
+
+numbers.shift();
+console.log(numbers)
+
+numbers.splice(1,1)
+console.log(numbers)
